edf33e6d86f0c6e7df0ff72662cf2a3e22f65c52
SrdjanCukic <cukic.srdjan@gmail.com>
2023-02-07 17:37:29 +0100

    API KEY UPDATED

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '76caef8e-c0c4-4095-ab6d-53d2c73dc1e7';
 export const MODAL_CLOSE_SEC = 2.5;
